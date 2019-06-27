059417e9b427a6c9ea8736eed618727066b29db3
NenadJov <52101094+NenadJov@users.noreply.github.com>
2019-06-28 00:48:17 +0200

    potrosuvacka na gorivo

---
+++ potrosuvacka na gorivo.js
@@ -0,0 +1,11 @@
+function gasSpent(km){
+    // 7 : 100 = x : 43
+    // x = 7 * 43 / 100
+    x = km
+    for (i = 0; i <= x; i++){
+        var potrosnjapokm = 7 / 100;
+        var vkpotrosnja = i * potrosnjapokm;
+    }
+    console.log(vkpotrosnja);
+}
+gasSpent(43);
